f4e81b7bc6444894808bf13cf68d8a397522e2c9
Gatien <gatieng2005@gmail.com>
2023-02-04 17:03:30 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -5,24 +5,65 @@ import json
 # Serveur => 1
 # Client => 2
 
+
 class Client:
     def __init__(self, ip):
         self.socket = socket()
-        self.socket.connect((ip , 8080))
+        self.socket.connect((ip, 8080))
         self.grille = [[0]*3]*3
         self.socket.send(str(self.grille).encode())
-        self.grille = json.loads(self.socket.recv(1000).decode())
-        self.affichage()
-        self.cocher()
-        
+        self.main()
+
+    def main(self):
+        while not self.hasWon():
+            data = self.socket.recv(1000).decode()
+            self.grille = json.loads(data)
+            if data == "end":
+                print("""
+=============
+TU AS PERDU !
+=============
+                """)
+                return
+            self.affichage()
+            self.cocher()
+            self.socket.send(str(self.grille).encode())
+        self.socket.send("end".encode())
+
     def cocher(self):
         choix = input('Entrer la case a jouer (ex : a2): ')
         coup = (ord(choix[0].lower())-97, int(choix[1])-1)
         if self.grille[coup[0]][coup[1]] == 0:
-            self.grille[coup[0]][coup[1]] = 1
-            self.socket.send(str(self.grille).encode())
+            self.grille[coup[0]][coup[1]] = 2
         else:
             self.cocher()
-    
+
+    def hasWon(self) -> bool:
+        grille = self.grille
+        # check ligne
+        for ligne in self.grille:
+            if ligne == [2]*3:
+                return True
+        
+        # check colonne
+        for i in range(3):
+            liste = []
+            for k in range(3):
+                liste += [grille[k][i]]
+            if liste == [2]*3:
+                return True
+        
+        # check diagonale
+        liste = []
+        for i in range(3):
+            liste += [grille[i][i]]
+            liste += [grille[2-i][i]]
+        for diagonale in liste:
+            if diagonale == [2]*3:
+                return True
+        
+        return False
+
+
     def affichage(self):
         print(self.grille)
--- serveur.py
+++ serveur.py
@@ -11,6 +11,7 @@ class Serveur:
         self.ip = socket.gethostbyname(socket.gethostname())
         self.socket = socket.socket()
         self.socket.bind((self.ip, port))
+        self.grille = [[0]*3]*3
         self.socket.listen()
         print(f'''
 =====================
@@ -19,18 +20,59 @@ PARTIE DISPONIBLE sur
 =====================
         ''')
         (sclient,adclient) = self.socket.accept()
-        self.grille = json.loads(sclient.recv(1024).decode())
-        self.affichage()
-        self.cocher(sclient)
+        self.sclient = sclient
+        self.main()
     
-    def cocher(self, sclient):
+    def main(self):
+        while not self.hasWon():
+            data = self.sclient.recv(1024).decode()
+            self.grille = json.loads(data)
+            if data == "end":
+                print("""
+=============
+TU AS PERDU !
+=============
+                """)
+                return
+            self.affichage()
+            self.cocher()
+            self.sclient.send(str(self.grille).encode())
+        self.sclient.send("end".encode())
+        
+
+    def cocher(self):
         choix = input('Entrer la case a jouer (ex : a2): ')
         coup = (ord(choix[0].lower())-97, int(choix[1])-1)
         if self.grille[coup[0]][coup[1]] == 0:
             self.grille[coup[0]][coup[1]] = 1
-            sclient.send(str(self.grille).encode())
         else:
             self.cocher()
 
     def affichage(self):
         print(self.grille)
+    
+    def hasWon(self) -> bool:
+        grille = self.grille
+        # check ligne
+        for ligne in self.grille:
+            if ligne == [1]*3:
+                return True
+        
+        # check colonne
+        for i in range(3):
+            liste = []
+            for k in range(3):
+                liste += [grille[k][i]]
+            if liste == [1]*3:
+                return True
+        
+        # check diagonale
+        liste = []
+        for i in range(3):
+            liste += [grille[i][i]]
+            liste += [grille[2-i][i]]
+        for diagonale in liste:
+            if diagonale == [1]*3:
+                return True
+        
+        return False
